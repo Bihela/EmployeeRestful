73d5020daec8a28a2dcdd419fea9ba13d0ba0e52
Bihela <sandeshwanasekara@gmail.com>
2024-04-02 16:31:10 +0530

    Start of the Feature, incomplete the code

---
+++ EmployeeManagement.Web/Components/Pages/EmployeeListBase.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Components.Pages
+{
+	public class EmployeeListBase : ComponentBase
+	{
+		public IEnumerable<Employee> Employees { get; set; }
+
+	}
+	private void LoadEmployees()
+	{
+		Employee e1 = new Employee
+		{
+			EmployeeId = 1,
+			FirstName = "John",
+			LastName = "Hastings",
+			Email = "David@inteprid.com",
+			DateOfBirth = new DateTime(1980, 10 ,5),
+			Gender = Gender.Male,
+			Department = New Department { DepartmentId = 1, },
+}
