4a87a9a66fedcf2134403c0680397ff53e21c8ab
Bihela <sandeshwanasekara@gmail.com>
2024-04-03 13:53:52 +0530

    Added the Employee Details into LocalDB

--- EmployeeManagement.Models/obj/Debug/net8.0/EmployeeManagement.Models.AssemblyInfo.cs
+++ EmployeeManagement.Models/obj/Debug/net8.0/EmployeeManagement.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+146e02cc0e6156c1ad8848434821d3e8e8bfa3d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa133ce9481480f1fc8d7d0f1796ce22a2d6d3d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EmployeeManagment.Api/Program.cs
+++ EmployeeManagment.Api/Program.cs
@@ -1,9 +1,19 @@
+using System;
+using EmployeeManagment.Api.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
